adea6f5300a8a94cee766f408d93421942091fc0
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-06 15:31:12 +0200

    zona jugable

--- src/tablero/semicirculo-tablero.jsx
+++ src/tablero/semicirculo-tablero.jsx
@@ -4,14 +4,25 @@ function Semicirculo(){
   return (
     <>
     <div className='contenedor-principal'>
+      <div className='circle5'></div>
       <ul className="circle">
         <li className='p1'><div className="text"></div></li>
         <li className='p2'><div className="text"></div></li>
         <li className='p3'><div className="text"></div></li>
-        <li className='p4'><div className="text"></div></li>
+        <li className='p4'>
+          <div className="text">
+            <div className='tie-section'></div>
+            <div className='banker-section'></div>
+            <div className='player-section'></div>
+          </div>
+        </li>
         <li className='p5'><div className="text"></div></li>
         <li className='p6'><div className="text"></div></li>
       </ul>
+      <div className='circle2'>
+        <div className='circle3'></div>
+        <div className='circle4'></div>
+      </div>
     </div>
     </>
   )
