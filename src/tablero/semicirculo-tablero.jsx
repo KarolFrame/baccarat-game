eef0a02eef047ba7bb3b7a05ad1fb855f1b10a50
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-06 14:01:57 +0200

    circulo

--- src/tablero/semicirculo-tablero.jsx
+++ src/tablero/semicirculo-tablero.jsx
@@ -3,10 +3,15 @@ import './semicirculo-tablero.css'
 function Semicirculo(){
   return (
     <>
-    <div className='semicirculo-tablero'>
-      <div className='segundo-semicirculo'></div>
-      <div className='tercer-semicirculo'></div>
-      <div className='cuarto-semicirculo'></div>
+    <div className='contenedor-principal'>
+      <ul className="circle">
+        <li><div className="text">1</div></li>
+        <li><div className="text">2</div></li>
+        <li><div className="text">3</div></li>
+        <li><div className="text">4</div></li>
+        <li><div className="text">5</div></li>
+        <li><div className="text">6</div></li>
+      </ul>
     </div>
     </>
   )
