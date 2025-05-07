b4273c23f7b3480c3748ef2cc5bb690d3b222755
Carolina <carolina.peseca@gmail.com>
2025-05-07 02:55:00 +0200

    creacion del componente Card

---
+++ src/tablero/card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./card.css"
+
+const symbols = {
+  hearts: '♥',
+  diamonds: '♦',
+  clubs: '♣',
+  spades: '♠',
+};
+
+function Card({ number, suit, top = 0, left = 0 }) {
+  const symbols = {
+    hearts: '♥',
+    diamonds: '♦',
+    clubs: '♣',
+    spades: '♠',
+  };
+
+  const symbol = symbols[suit];
+  const color = suit === 'hearts' || suit === 'diamonds' ? 'red' : 'black';
+
+  return (
+    <div className="card" style={{ top: `${top}px`, left: `${left}px` }}>
+      <p className={`card-text card-number ${color}`}>{number} <br /> {symbol}</p>
+      <p className={`card-symbol ${color}`}>{symbol}</p>
+      <p className={`card-text card-number upside-down ${color}`}>{number} <br /> {symbol}</p>
+    </div>
+  );
+}
+
+export default Card;
